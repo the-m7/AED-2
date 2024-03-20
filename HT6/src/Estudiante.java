c3f83477eda7ac7eadbf4bcdfa71098e1a675281
the-m7 <martinperezbet@gmail.com>
2024-03-19 18:45:02 -0600

    clase estudiantes

---
+++ HT6/src/Estudiante.java
@@ -0,0 +1,14 @@
+public class Estudiante {
+    String Name, Phone, Email, County;
+    Integer PostalZip;
+    
+    public Estudiante(String name, String phone, String email, String county, Integer postalZip) {
+        Name = name;
+        Phone = phone;
+        Email = email;
+        County = county;
+        PostalZip = postalZip;
+    }
+
+    
+}
