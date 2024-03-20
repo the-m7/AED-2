f8781f75e34610d26078087847293ad6ad051f6a
the-m7 <martinperezbet@gmail.com>
2024-03-20 11:18:00 -0600

    cambios jsonread

--- HT6/src/JSONRead.java
+++ HT6/src/JSONRead.java
@@ -2,14 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.json.simple.JSONObject; 
-import com.fasterxml.jackson.databind.ObjectMapper
 
 public class JSONRead {
-    // Método para leer un archivo JSON y convertirlo en un objeto Java
-    public static <T> T readJSONFile(String filePath, Class<T> valueType) throws IOException {
-        // Crear un objeto ObjectMapper de Jackson
-        ObjectMapper objectMapper = new ObjectMapper();
-        // Leer el archivo JSON y convertirlo en un objeto Java del tipo especificado
-        return objectMapper.readValue(new File(filePath), valueType);
-    }
+    
 }
