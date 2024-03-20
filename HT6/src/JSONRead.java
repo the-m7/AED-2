208e2efdf3bbe4ededf32fd7e0accf12fa7348c7
the-m7 <martinperezbet@gmail.com>
2024-03-20 10:07:04 -0600

    no sirve el json

--- HT6/src/JSONRead.java
+++ HT6/src/JSONRead.java
@@ -1,12 +1,15 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import org.json.simple.JSONObject; 
+import com.fasterxml.jackson.databind.ObjectMapper
 
 public class JSONRead {
-    public static ArrayList<Estudiante> read(String fileName){
-        
-        JSONObject jo = new JSONParser().parse(new FileReader(fileName));
-        
-        return null;
-
+    // Método para leer un archivo JSON y convertirlo en un objeto Java
+    public static <T> T readJSONFile(String filePath, Class<T> valueType) throws IOException {
+        // Crear un objeto ObjectMapper de Jackson
+        ObjectMapper objectMapper = new ObjectMapper();
+        // Leer el archivo JSON y convertirlo en un objeto Java del tipo especificado
+        return objectMapper.readValue(new File(filePath), valueType);
     }
 }
