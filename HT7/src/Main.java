04d066f9b7af7eef750273c71ce9a49505504836
the-m7 <martinperezbet@gmail.com>
2024-04-02 21:41:41 -0600

    Main con traducción del texto y detección del idioma

--- HT7/src/Main.java
+++ HT7/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -45,13 +46,13 @@ public class Main {
             System.out.println(" 1. Ingles");
             System.out.println(" 2. Español");
             System.out.println(" 3. Frances");
-            System.out.println(" 4. Detectar Automaticamente (no implementado)");
             System.out.println("Ingrese el indice de la opción que desea");
             
             to_sel = scan.nextInt()-1;
             
             scan.close();
-            if(from_sel >= 0 && from_sel <= 2){
+            
+            if(from_sel >= 0 && from_sel <= 3){
                 BinarySearchTree<String,String[]> selTree = null;
     
                 switch(from_sel){
@@ -64,6 +65,59 @@ public class Main {
                     case 2:
                     selTree = TreeFrench;
                     break;
+                    case 3:
+
+                    int contE = 0;
+                    int contS = 0;
+                    int contF = 0;
+
+                    for(String LineaO : Texto){
+                        String[] LineaO2 = LineaO.split(" ");
+                        for(String Palabra : LineaO2){
+                            Palabra = Palabra.strip();
+                            if(TreeEnglish.find(Palabra)!=null){
+                                contE++;
+                            }
+                            if(TreeSpanish.find(Palabra)!=null){
+                                contS++;
+                            }
+                            if(TreeFrench.find(Palabra)!=null){
+                                contF++;
+                            }
+
+                        }
+                    }
+                    if(contE>contS){
+                        selTree=TreeEnglish;
+                    } else if(contS>contF){
+                        selTree=TreeSpanish;
+                    } else {
+                        selTree=TreeFrench;
+                    }
+                    break;
+
+                }
+
+                ArrayList<String> TextoTrad = new ArrayList<String>();
+
+                for(String Linea : Texto){
+                    String[] Linea2 = Linea.split(" ");
+                    String LineaTrad = "";
+                    
+                    for(String Palabra : Linea2){
+                        Palabra = Palabra.strip();
+                        
+                        String[] PalTradL = selTree.find(Palabra);
+
+                        if(PalTradL!=null){
+                            String PalTrad = PalTradL[to_sel];
+                            LineaTrad += PalTrad + " ";
+                        } else {
+                            LineaTrad += "*" + Palabra + "* ";
+                        }
+                    }
+                    System.out.println(LineaTrad);
+                    TextoTrad.add(LineaTrad);
                 }
 
             }
