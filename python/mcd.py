1b18658ac783ca22f444ad15f46d4c506f926fb7
the-m7 <martinperezbet@gmail.com>
2024-04-20 13:31:20 -0600

    cosas de python

--- python/mcd.py
+++ python/mcd.py
@@ -1,20 +1,54 @@
-def mcd(a,b):
-    r = a % b
-    if r == 0: return b
-    else: return mcd(b,r)
+class app():
 
-def mcd2(a,b):
-    r = a%b
-    while(r != 0):
-        a = b
-        b = r
+    def mcd(self,a,b):
+        r = a % b
+        if r == 0: return b
+        else: return self.mcd(b,r)            
+
+    def mcd2(self,a,b):
         r = a%b
-    return b
-        
+        while(r != 0):
+            q = a//b
 
-a = 437
-b = 57
+            self.Ls.append(self.Ls[-2]-q*self.Ls[-1])
+            self.Lt.append(self.Lt[-2]-q*self.Lt[-1])
 
-print("El mcd de",a,"y",b,"es")
-print("Con recursividad:",mcd(a,b))
-print("Con ciclos:",mcd2(a,b))
+            a = b
+            b = r
+            r = a%b
+        return b
+            
+    def __init__(self):
+        print("ax + by = c")
+
+        a = int(input("ingrese el valor de a: "))
+        b = int(input("ingrese el valor de b: "))
+        c = int(input("ingrese el valor de c: "))
+
+        self.Ls = [1,0]
+        self.Lt = [0,1]
+
+        d = self.mcd2(a,b)
+
+        print(f"\n{a}x + {b}y = {c}")
+
+        print("\nEl mcd de",a,"y",b,"es")
+        print("Con recursividad:",self.mcd(a,b))
+        print("Con ciclos:",d)
+
+        if(c%d==0):
+
+            # print("s =",self.Ls)
+            # print("t =",self.Lt)
+
+            x=self.Ls[-1] * c//d
+            y=self.Lt[-1] * c//d
+
+            print(f"\n{a} * ({x}) + {b} * ({y}) =", a * x + b * y)
+
+            print(f"x = {x} + {b//d}k")
+            print(f"y = {y} - {a//d}k")
+
+        else:
+            print("la ecuación no tiene solución")
+app()
