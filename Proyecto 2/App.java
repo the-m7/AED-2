e596772d00f0351ae5ca4e792f87e5802d65b8d3
karen23692 <tol23692@uvg.edu.gt>
2024-05-26 01:41:46 -0600

    confirmando conexión a la base de datos en Sandbox

---
+++ Proyecto 2/App.java
@@ -0,0 +1,67 @@
+
+/**
+ * 
+ */
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // Connexión a la base en Sandbox
+        String username = "neo4j";
+        String password = "technology-secretary-repair";
+        String boltURL = "bolt://3.84.219.222";
+
+        try (EmbeddedNeo4j db = new EmbeddedNeo4j(boltURL, username, password)) {
+            LinkedList<String> myusers = db.getUsers();
+
+            for (int i = 0; i < myusers.size(); i++) {
+                System.out.println(myusers.get(i));
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Ingrese un interes:");
+        Scanner in = new Scanner(System.in);
+        String myInterest = in.nextLine();
+
+        try (EmbeddedNeo4j db = new EmbeddedNeo4j(boltURL, username, password)) {
+            LinkedList<String> users = db.getUsersByInterest(myInterest);
+
+            for (int i = 0; i < users.size(); i++) {
+                System.out.println(users.get(i));
+
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Ingrese una nueva Pelicula");
+        System.out.println("Ingrese Nombre de la pelicula");
+        String movieTitle = in.nextLine();
+        System.out.println("Ingrese A�o Lanzamiento");
+        int movieReleaseYear = Integer.parseInt(in.nextLine());
+        System.out.println("Ingrese Descripcion");
+        String movieTagLine = in.nextLine();
+
+        try (EmbeddedNeo4j db = new EmbeddedNeo4j(boltURL, username, password)) {
+            String result = db.insertUser(movieTitle, movieReleaseYear, movieTagLine);
+
+            if (result.equalsIgnoreCase("OK")) {
+                System.out.println("Pelicula insertada correctamente");
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
--- Proyecto 2/EmbeddedNeo4j.java
+++ Proyecto 2/EmbeddedNeo4j.java
@@ -1,4 +1,3 @@
-
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
@@ -14,11 +13,6 @@ import static org.neo4j.driver.Values.parameters;
 import java.util.LinkedList;
 import java.util.List;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.neo4j.driver.Values.parameters;
-
 public class EmbeddedNeo4j implements AutoCloseable {
 
     private final Driver driver;
@@ -31,105 +25,66 @@ public class EmbeddedNeo4j implements AutoCloseable {
     public void close() throws Exception {
         driver.close();
     }
-    // NO estoy seguro que va aqui.
 
-    // public void printGreeting( final String message )
-    // {
-    // try ( Session session = driver.session() )
-    // {
-    // String greeting = session.writeTransaction( new TransactionWork<String>()
-    // {
-    // @Override
-    // public String execute( Transaction tx )
-    // {
-    // Result result = tx.run( "CREATE (a:Greeting) " +
-    // "SET a.message = $message " +
-    // "RETURN a.message + ', from node ' + id(a)",
-    // parameters( "message", message ) );
-    // return result.single().get( 0 ).asString();
-    // }
-    // } );
-    // System.out.println( greeting );
-    // }
-    // Los nodos se ingresan en linkedLists
+    public void printGreeting(final String message) {
+        try (Session session = driver.session()) {
+            String greeting = session.writeTransaction(new TransactionWork<String>() {
+                @Override
+                public String execute(Transaction tx) {
+                    Result result = tx.run("CREATE (a:Greeting) " +
+                            "SET a.message = $message " +
+                            "RETURN a.message + ', from node ' + id(a)",
+                            parameters("message", message));
+                    return result.single().get(0).asString();
+                }
+            });
+            System.out.println(greeting);
+        }
+    }
 
-    public LinkedList<String> getPerson() {
+    public LinkedList<String> getUsers() {
         try (Session session = driver.session()) {
 
-            LinkedList<String> people = session.readTransaction(new TransactionWork<LinkedList<String>>() {
+            LinkedList<String> users = session.readTransaction(new TransactionWork<LinkedList<String>>() {
                 @Override
                 public LinkedList<String> execute(Transaction tx) {
                     // Result result = tx.run( "MATCH (people:Person) RETURN people.name");
                     Result result = tx.run("MATCH (people:Person) RETURN people.name");
-                    LinkedList<String> mypeople = new LinkedList<String>();
+                    LinkedList<String> myusers = new LinkedList<String>();
                     List<Record> registros = result.list();
                     for (int i = 0; i < registros.size(); i++) {
-                        // mypeople.add(registros.get(i).toString());
-                        mypeople.add(registros.get(i).get("people.name").asString());
+                        myusers.add(registros.get(i).get("people.name").asString());
                     }
 
-                    return mypeople;
+                    return myusers;
                 }
             });
 
-            return people;
+            return users;
         }
     }
 
-    public LinkedList<String> getInterest() {
+    public LinkedList<String> getUsersByInterest(String interst) {
         try (Session session = driver.session()) {
 
-            LinkedList<String> interests = session.readTransaction(new TransactionWork<LinkedList<String>>() {
+            LinkedList<String> users = session.readTransaction(new TransactionWork<LinkedList<String>>() {
                 @Override
                 public LinkedList<String> execute(Transaction tx) {
-                    // Result result = tx.run( "MATCH (people:Person) RETURN people.name");
-                    Result result = tx.run("MATCH (interest:Interest) RETURN interest.name");
-                    LinkedList<String> myinterests = new LinkedList<String>();
+                    Result result = tx.run("MATCH (interest:Interest {name:\"" + interst
+                            + "\"})<-[:LIKES]-(people:Person) RETURN people.name;");
+                    LinkedList<String> myusers = new LinkedList<String>();
                     List<Record> registros = result.list();
-                    for (int i = 0; i < registros.size(); i++) {
-                        // mypeople.add(registros.get(i).toString());
-                        myinterests.add(registros.get(i).get("interest.name").asString());
+                    for (int i = 0; i < registros.size() / 2; i++) {
+                        myusers.add(registros.get(i).get("people.name").asString());
                     }
-
-                    return myinterests;
+                    return myusers;
                 }
             });
-
-            return interests;
+            return users;
         }
     }
 
-    // En los siguientes métodos se busca a la persona por gustos, características o
-    // género
-    // public LinkedList<String> getPersonByInterest(String Interest)
-    // {
-    // try ( Session session = driver.session() )
-    // {
-
-    // LinkedList<String> actors = session.readTransaction( new
-    // TransactionWork<LinkedList<String>>()
-    // {
-    // @Override
-    // public LinkedList<String> execute( Transaction tx )
-    // {
-    // Result result = tx.run( "MATCH (tom:Person {name: \"" + actor +
-    // "\"})-[:ACTED_IN]->(actorMovies) RETURN actorMovies.title");
-    // LinkedList<String> myactors = new LinkedList<String>();
-    // List<Record> registros = result.list();
-    // for (int i = 0; i < registros.size(); i++) {
-    // //myactors.add(registros.get(i).toString());
-    // myactors.add(registros.get(i).get("actorMovies.title").asString());
-    // }
-
-    // return myactors;
-    // }
-    // } );
-
-    // return actors;
-    // }
-    // }
-
-    public String insertMovie(String title, int releaseYear, String tagline) {
+    public String insertUser(String name, int age, String tagline) {
         try (Session session = driver.session()) {
 
             String result = session.writeTransaction(new TransactionWork<String>()
@@ -137,7 +92,7 @@ public class EmbeddedNeo4j implements AutoCloseable {
             {
                 @Override
                 public String execute(Transaction tx) {
-                    tx.run("CREATE (Test:Movie {title:'" + title + "', released:" + releaseYear + ", tagline:'"
+                    tx.run("CREATE (Test:Movie {title:'" + name + "', released:" + age + ", tagline:'"
                             + tagline + "'})");
 
                     return "OK";
