5477179dd57f5f50905315257bd2c265abc863ee
karen23692 <tol23692@uvg.edu.gt>
2024-05-25 23:01:53 -0600

    librerias importadas :D

--- Proyecto 2/EmbeddedNeo4j.java
+++ Proyecto 2/EmbeddedNeo4j.java
@@ -23,141 +23,126 @@ public class EmbeddedNeo4j implements AutoCloseable {
 
     private final Driver driver;
 
-
-    public EmbeddedNeo4j( String uri, String user, String password )
-    {
-        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    public EmbeddedNeo4j(String uri, String user, String password) {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
     }
 
     @Override
-    public void close() throws Exception
-    {
+    public void close() throws Exception {
         driver.close();
     }
-    //NO estoy seguro que va aqui.
+    // NO estoy seguro que va aqui.
 
-    //public void printGreeting( final String message )
-    //{
-     //   try ( Session session = driver.session() )
-       // {
-         //   String greeting = session.writeTransaction( new TransactionWork<String>()
-           // {
-             //   @Override
-               // public String execute( Transaction tx )
-             //   {
-                 //   Result result = tx.run( "CREATE (a:Greeting) " +
-                     //               "SET a.message = $message " +
-                   //                 "RETURN a.message + ', from node ' + id(a)",
-                       //     parameters( "message", message ) );
-                //    return result.single().get( 0 ).asString();
-              //  }
-           // } );
-           // System.out.println( greeting );
-        //}
-    }
+    // public void printGreeting( final String message )
+    // {
+    // try ( Session session = driver.session() )
+    // {
+    // String greeting = session.writeTransaction( new TransactionWork<String>()
+    // {
+    // @Override
+    // public String execute( Transaction tx )
+    // {
+    // Result result = tx.run( "CREATE (a:Greeting) " +
+    // "SET a.message = $message " +
+    // "RETURN a.message + ', from node ' + id(a)",
+    // parameters( "message", message ) );
+    // return result.single().get( 0 ).asString();
+    // }
+    // } );
+    // System.out.println( greeting );
+    // }
+    // Los nodos se ingresan en linkedLists
 
-    //Los nodos se ingresan en linkedLists
-    public LinkedList<String> getPerson()
-    {
-        try ( Session session = driver.session() )
-        {
+    public LinkedList<String> getPerson() {
+        try (Session session = driver.session()) {
 
-
-            LinkedList<String> people = session.readTransaction( new TransactionWork<LinkedList<String>>()
-            {
+            LinkedList<String> people = session.readTransaction(new TransactionWork<LinkedList<String>>() {
                 @Override
-                public LinkedList<String> execute( Transaction tx )
-                {
-                    //Result result = tx.run( "MATCH (people:Person) RETURN people.name");
-                    Result result = tx.run( "MATCH (people:Person) RETURN people.name");
+                public LinkedList<String> execute(Transaction tx) {
+                    // Result result = tx.run( "MATCH (people:Person) RETURN people.name");
+                    Result result = tx.run("MATCH (people:Person) RETURN people.name");
                     LinkedList<String> mypeople = new LinkedList<String>();
                     List<Record> registros = result.list();
                     for (int i = 0; i < registros.size(); i++) {
-                        //mypeople.add(registros.get(i).toString());
+                        // mypeople.add(registros.get(i).toString());
                         mypeople.add(registros.get(i).get("people.name").asString());
                     }
 
                     return mypeople;
                 }
-            } );
+            });
 
             return people;
         }
     }
 
-    public LinkedList<String> getInterest()
-    {
-     try ( Session session = driver.session() )
-        {
+    public LinkedList<String> getInterest() {
+        try (Session session = driver.session()) {
 
-
-            LinkedList<String> interests = session.readTransaction( new TransactionWork<LinkedList<String>>()
-            {
+            LinkedList<String> interests = session.readTransaction(new TransactionWork<LinkedList<String>>() {
                 @Override
-                public LinkedList<String> execute( Transaction tx )
-                {
-                    //Result result = tx.run( "MATCH (people:Person) RETURN people.name");
-                    Result result = tx.run( "MATCH (interest:Interest) RETURN interest.name");
+                public LinkedList<String> execute(Transaction tx) {
+                    // Result result = tx.run( "MATCH (people:Person) RETURN people.name");
+                    Result result = tx.run("MATCH (interest:Interest) RETURN interest.name");
                     LinkedList<String> myinterests = new LinkedList<String>();
                     List<Record> registros = result.list();
                     for (int i = 0; i < registros.size(); i++) {
-                        //mypeople.add(registros.get(i).toString());
+                        // mypeople.add(registros.get(i).toString());
                         myinterests.add(registros.get(i).get("interest.name").asString());
-                }
+                    }
 
                     return myinterests;
                 }
-            } );
+            });
 
             return interests;
         }
     }
 
+    // En los siguientes métodos se busca a la persona por gustos, características o
+    // género
+    // public LinkedList<String> getPersonByInterest(String Interest)
+    // {
+    // try ( Session session = driver.session() )
+    // {
 
+    // LinkedList<String> actors = session.readTransaction( new
+    // TransactionWork<LinkedList<String>>()
+    // {
+    // @Override
+    // public LinkedList<String> execute( Transaction tx )
+    // {
+    // Result result = tx.run( "MATCH (tom:Person {name: \"" + actor +
+    // "\"})-[:ACTED_IN]->(actorMovies) RETURN actorMovies.title");
+    // LinkedList<String> myactors = new LinkedList<String>();
+    // List<Record> registros = result.list();
+    // for (int i = 0; i < registros.size(); i++) {
+    // //myactors.add(registros.get(i).toString());
+    // myactors.add(registros.get(i).get("actorMovies.title").asString());
+    // }
 
-    //En los siguientes métodos se busca a la persona por gustos, características o género
-    public LinkedList<String> getPersonByInterest(String Interest)
-    {
-        try ( Session session = driver.session() )
-        {
+    // return myactors;
+    // }
+    // } );
 
-
-            LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
-            {
-                @Override
-                public LinkedList<String> execute( Transaction tx )
-                {
-                    Result result = tx.run( "MATCH (tom:Person {name: \"" + actor + "\"})-[:ACTED_IN]->(actorMovies) RETURN actorMovies.title");
-                    LinkedList<String> myactors = new LinkedList<String>();
-                    List<Record> registros = result.list();
-                    for (int i = 0; i < registros.size(); i++) {
-                        //myactors.add(registros.get(i).toString());
-                        myactors.add(registros.get(i).get("actorMovies.title").asString());
-                    }
-
-                    return myactors;
-                }
-            } );
-
-            return actors;
-        }
-    }
+    // return actors;
+    // }
+    // }
 
     public String insertMovie(String title, int releaseYear, String tagline) {
-        try ( Session session = driver.session() )
-        {
+        try (Session session = driver.session()) {
 
-            String result = session.writeTransaction( new TransactionWork<String>()
+            String result = session.writeTransaction(new TransactionWork<String>()
 
-                                                      {
-                                                          @Override
-                                                          public String execute( Transaction tx )
-                                                          {
-                                                              tx.run( "CREATE (Test:Movie {title:'" + title + "', released:"+ releaseYear +", tagline:'"+ tagline +"'})");
+            {
+                @Override
+                public String execute(Transaction tx) {
+                    tx.run("CREATE (Test:Movie {title:'" + title + "', released:" + releaseYear + ", tagline:'"
+                            + tagline + "'})");
 
-                                                              return "OK";
-                                                          }
-                                                      }
+                    return "OK";
+                }
+            }
 
             );
 
